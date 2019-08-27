13f7a1bee38d40fb19a5d92b860573725308d917
Philipp <ufedz@student.kit.edu>
2019-08-27 13:26:14 +0200

    Implemented classes to map variables and constraints to their respective layers to allow for optimization of bounds layer by layer

---
+++ performance.py
@@ -0,0 +1,305 @@
+
+from abc import ABC, abstractmethod
+from expression import Expression, Variable
+from keras_loader import KerasLoader
+from expression_encoding import encode_equivalence, interval_arithmetic, hasLinear, encode_linear_layer, \
+    encode_relu_layer, encode_one_hot, encode_ranking_layer, encode_equivalence_layer
+import gurobipy as grb
+
+
+class Layer(ABC):
+
+    def __init__(self, activation, num_neurons, invars, intervars, outvars, constraints):
+        self.activation = activation
+        self.num_neurons = num_neurons
+        self.invars = invars
+        self.outvars = outvars
+        self.intervars = intervars
+        self.constraints = constraints
+
+    def get_invars(self):
+        return self.invars
+
+    def get_invar(self, row):
+        return self.invars[row]
+
+    def get_intervars(self):
+        return self.intervars
+
+    def get_outvars(self):
+        return self.outvars
+
+    def get_outvar(self, row):
+        return self.outvars[row]
+
+    def get_all_vars(self):
+        return self.invars + self.intervars + self.outvars
+
+    def get_constraints(self):
+        return self.constraints
+
+    @abstractmethod
+    def get_optimization_vars(self):
+        pass
+
+    @abstractmethod
+    def get_optimization_constraints(self):
+        pass
+
+
+class DefaultLayer(Layer):
+
+    def __init__(self, activation, num_neurons, invars, intervars, outvars, constraints):
+        super(DefaultLayer, self).__init__(activation, num_neurons, invars, intervars, outvars, constraints)
+
+    def get_optimization_vars(self):
+        return self.outvars
+
+    def get_optimization_constraints(self):
+        return self.constraints
+
+
+class InputLayer(Layer):
+
+    def __init__(self, num_neurons, invars):
+        super(InputLayer, self).__init__('input', num_neurons, invars, [], invars, [])
+        self.invars = invars
+
+    def get_optimization_vars(self):
+        return []
+
+    def get_optimization_constraints(self):
+        return []
+
+    def get_all_vars(self):
+        return self.invars
+
+
+class ReLULayer(Layer):
+
+    def __init__(self, lin_layer, num_neurons, invars, intervars, outvars, constraints):
+        super(ReLULayer, self).__init__('relu', num_neurons, invars, intervars, outvars, constraints)
+        self.lin_layer = lin_layer
+        self.intervars = self.lin_layer.intervars + self.lin_layer.outvars + intervars
+
+    def get_optimization_vars(self):
+        return self.lin_layer.get_optimization_vars()
+
+    def get_optimization_constraints(self):
+        return self.lin_layer.get_optimization_constraints()
+
+
+class Encoder:
+
+    def __init__(self):
+        self.a_layers = []
+        self.b_layers = []
+        self.input_layer = None
+        self.equivalence_layer = None
+
+    def encode_inputs(self, lower_bounds, upper_bounds, netPrefix=''):
+        vars = []
+        for i, (l, h) in enumerate(zip(lower_bounds, upper_bounds)):
+            input_var = Variable(0, i, netPrefix, 'i')
+            input_var.setLo(l)
+            input_var.setHi(h)
+            vars.append(input_var)
+
+        num_neurons = len(lower_bounds)
+        return InputLayer(num_neurons, vars)
+
+    def encode_layers(self, input_vars, layers, net_prefix, output_mode=('matrix', -1)):
+        # outputs modes to specify what is output of ranking layer:
+        # ('matrix', -1) whole matrix is output
+        # ('matrix', k) matrix[0:k] is output
+        # ('out', -1) whole sorted vector is output
+
+        vars = []
+        constraints = []
+        net_layers = []
+
+        invars = input_vars
+        # output vars always appended last!
+        for i, (activation, num_neurons, weights) in enumerate(layers):
+            if hasLinear(activation):
+                linvars, eqs = encode_linear_layer(invars, weights, num_neurons, i, net_prefix)
+                vars.append(linvars)
+                constraints.append(eqs)
+
+                lin_layer = Layer('linear', num_neurons, invars, [], linvars, eqs)
+
+                if activation == 'relu':
+                    reluouts, reludeltas, reluineqs = encode_relu_layer(linvars, i, net_prefix)
+
+                    vars.append(reludeltas)
+                    vars.append(reluouts)
+                    constraints.append(reluineqs)
+
+                    net_layers.append(ReLULayer(lin_layer, num_neurons, invars, reludeltas, reluouts, reluineqs))
+
+                    invars = reluouts
+                elif activation == 'linear':
+                    net_layers.append(lin_layer)
+
+                    invars = linvars
+            else:
+                # just use weights = None for one_hot layer
+                if activation == 'one_hot':
+                    oh_outs, oh_vars, oh_constraints = encode_one_hot(invars, i, net_prefix)
+                    vars.append(oh_vars)
+                    vars.append(oh_outs)
+                    constraints.append(oh_constraints)
+
+                    net_layers.append(DefaultLayer('one_hot', num_neurons, invars, oh_vars, oh_outs, oh_constraints))
+
+                    invars = oh_outs
+
+                if activation == 'ranking':
+                    rank_perms, rank_vars, rank_constraints = encode_ranking_layer(invars, i, net_prefix)
+                    vars.append(rank_vars)
+                    # rank_perms is permutation matrix !!!
+                    vars.append(rank_perms)
+                    constraints.append(rank_constraints)
+
+                    # !!! not sure, what to take for output of ranking layer !!!
+                    descriptor, k = output_mode
+                    l = None
+                    if descriptor == 'matrix' and k == -1:
+                        l = DefaultLayer('ranking', num_neurons, invars, rank_vars, rank_perms, rank_constraints)
+                    if descriptor == 'matrix' and k >= 0:
+                        l = DefaultLayer('ranking', num_neurons, invars, rank_vars + rank_perms[k:],
+                                         rank_perms[:k], rank_constraints)
+                    elif descriptor == 'out':
+                        l = DefaultLayer('ranking', num_neurons, invars, rank_vars[:-num_neurons] + rank_perms,
+                                         rank_vars[-num_neurons:], rank_constraints)
+                    else:
+                        raise ValueError('output mode ({desc}, {num}) does not exist'.format(desc=descriptor, num=k))
+
+                    net_layers.append(l)
+                    invars = rank_perms
+
+        return net_layers
+
+    def append_compare_layer(self, layers1, layers2, compared):
+        _, num_outs1, _ = layers1[-1]
+        _, num_outs2, _ = layers2[-1]
+
+        if not num_outs1 == num_outs2:
+            raise ValueError("both NNs must have the same number of outputs")
+
+        if compared == 'one_hot':
+            oh_layer = ('one_hot', num_outs1, None)
+            layers1.append(oh_layer)
+            layers2.append(oh_layer)
+        elif compared in {'ranking', 'ranking_one_hot'} or compared.startswith('ranking_top_'):
+            # not sure what to specify as num_neurons (num of sorted outs or num of p_ij in permutation matrix?)
+            ranking_layer = ('ranking', num_outs1, None)
+            layers1.append(ranking_layer)
+            layers2.append(ranking_layer)
+        elif compared.startswith('one_ranking_top_') or compared.startswith('optimize_ranking_top_'):
+            # not sure what to specify as num_neurons (num of sorted outs or num of p_ij in permutation matrix?)
+            ranking_layer = ('ranking', num_outs1, None)
+            layers1.append(ranking_layer)
+
+        return layers1, layers2
+
+    def determine_output_modes(self, compared):
+        # should never be used
+        mode1 = ('', -1)
+        mode2 = ('', -1)
+        if compared in {'outputs', 'one_hot'}:
+            mode1 = ('', -1)
+            mode2 = ('', -1)
+        elif compared == 'ranking_one_hot':
+            # matrix[0:1] == matrix[0]
+            mode1 = ('matrix', 1)
+            mode2 = ('matrix', 1)
+        elif compared.startswith('ranking_top_'):
+            k = int(compared.split('_')[-1])
+            mode1 = ('matrix', 1)
+            mode2 = ('matrix', k)
+        elif compared.startswith('one_ranking_top_') or compared.startswith('optimize_ranking_top_'):
+            mode1 = ('matrix', -1)
+            mode2 = ('', -1)
+        else:
+            # default case
+            raise ValueError('There is no ' + compared + ' keyword for param compared!!!')
+
+        return mode1, mode2
+
+    def encode_equivalence(self, layers1, layers2, input_lower_bounds, input_upper_bounds, compared='outputs',
+                           comparator='diff_zero'):
+        '''
+        :param layers1: first neural network as a list of layers of form (activation, num_neurons, weights)
+        :param layers2: second neural network as a list of layers of form (activation, num_neurons, weights)
+        :param input_lower_bounds: list of lower bounds for the input values
+        :param input_upper_bounds: list of upper bounds for the input values
+        :param compared: keyword for which element of the NNs should be compared.
+            outputs - compares the outputs of NN1 and NN2 directly,
+            one_hot - compares one-hot vectors of NN1 and NN2 generated from their output,
+            ranking_top_k - checks, whether greatest output of NN1 is within top k outputs of NN2 (k is a natural number)
+            ranking - (not supported yet) compares ranking vectors of NN1 and NN2 generated from their output,
+            ranking_one_hot - compares one-hot vectors of NN1 and NN2 generated from a permutation matrix
+            one_ranking_top_k - calculates one permutation matrix on outputs of NN1 and checks, for sortedness between
+                                top k outputs of NN2 and the rest of the outputs
+            optimize_ranking_top_k - calculates one permutation matrix on outputs of NN1 and checks for sortedness between
+                                top k outputs of NN2 and rest of outputs by computing the difference between o_1' and the non
+                                o_k+1'... outputs, needs manual optimization function
+        :param comparator: keyword for how the selected elements should be compared.
+            diff_zero    - elements should be equal
+            epsilon_e   - elements of output vector of NN2 should not differ by more than epsilon from the
+                            respective output element of NN1. Epsilon is equal to e (any positive number entered)
+            diff_one_hot - one-hot vectors should be equal (only works for one-hot encoding)
+            ranking_top_k - one-hot vector of NN1 should be within top k ranked outputs of NN2
+            ranking      - ???
+            one_ranking_top_k - calculates one permutation matrix on outputs of NN1 and checks, for sortedness between
+                                top k outputs of NN2 and the rest of the outputs
+            optimize_ranking_top_k - calculates one permutation matrix on outputs of NN1 and checks for sortedness between
+                                top k outputs of NN2 and rest of outputs by computing the difference between o_1' and the non
+                                o_k+1'... outputs, needs manual optimization function
+        :return: encoding of the equivalence of NN1 and NN2 as a set of variables and
+            mixed integer linear programming constraints
+        '''
+
+        layers1, layers2 = self.append_compare_layer(layers1, layers2, compared)
+        mode1, mode2 = self.determine_output_modes(compared)
+
+        self.input_layer = self.encode_inputs(input_lower_bounds, input_upper_bounds)
+        self.a_layers = self.encode_layers(self.input_layer.get_outvars(), layers1, 'A', mode1)
+        self.b_layers = self.encode_layers(self.input_layer.get_outvars(), layers2, 'B', mode2)
+
+        # only need output for optimize_ranking_top_k
+        outs1 = self.a_layers[-1].get_outvars()
+        outs2 = self.b_layers[-1].get_outvars()
+        eq_deltas, eq_diffs, eq_constraints = encode_equivalence_layer(outs1, outs2, comparator)
+
+        self.equivalence_layer = DefaultLayer('equiv', -1, outs1 + outs2, eq_deltas, eq_diffs, eq_constraints)
+
+    def get_vars(self):
+        input_vars = self.input_layer.get_all_vars()
+        net1_vars = [layer.get_all_vars() for layer in self.a_layers]
+        net2_vars = [layer.get_all_vars() for layer in self.b_layers]
+        equiv_vars = self.equivalence_layer.get_all_vars()
+
+        return input_vars + net1_vars + net2_vars + equiv_vars
+
+    def get_constraints(self):
+        # right now input layer has no constraints
+        input_constraints = self.input_layer.get_constraints()
+        net1_constraints = [layer.get_constraints() for layer in self.a_layers]
+        net2_constraints = [layer.get_constraints() for layer in self.b_layers]
+        equiv_constraints = self.equivalence_layer.get_constraints()
+
+        return input_constraints + net1_constraints + net2_constraints + equiv_constraints
+
+    def encode_equivalence_from_file(self, path1, path2, input_lower_bounds, input_upper_bounds, compared='outputs',
+                                     comparator='diff_zero'):
+        kl1 = KerasLoader()
+        kl1.load(path1)
+        layers1 = kl1.getHiddenLayers()
+
+        kl2 = KerasLoader()
+        kl2.load(path2)
+        layers2 = kl2.getHiddenLayers()
+
+        self.encode_equivalence(layers1, layers2, input_lower_bounds, input_upper_bounds, compared, comparator)
