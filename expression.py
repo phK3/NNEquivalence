fe57001679248459c5906b72c1d3935384bdc60b
Philipp <ufedz@student.kit.edu>
2019-09-25 13:52:06 +0200

    Tightened bigM encoding of 5th inequality in ReLU encoding.
    Upper bound in input is sufficient, as output can't be greater than input

--- expression.py
+++ expression.py
@@ -396,7 +396,8 @@ class Relu(Expression):
             model.addConstr(self.input.to_gurobi(model) + (1 - self.delta.to_gurobi(model)) * -m_input
                             >= 0, name=c_name + '_d')
 
-            M_active = self.output.getHi() - self.input.getLo()
+            #should not be < 0, because of if-else structure
+            M_active = self.input.getHi()
             model.addConstr(self.output.to_gurobi(model)
                             <= self.input.to_gurobi(model) + (1 - self.delta.to_gurobi(model)) * M_active, name=c_name + '_e')
 
